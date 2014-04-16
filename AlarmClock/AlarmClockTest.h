b3cb195a25d53d7038bfa2def72ffeb334d55ed5
Uma Shanker Tiwari <macmini2@MacMini2.local>
2014-04-16 12:33:36 +0530

    added my alarm test

---
+++ AlarmClock/AlarmClockTest.h
@@ -0,0 +1,13 @@
+//
+//  AlarmClockTest.h
+//  AlarmClock
+//
+//  Created by Uma Shanker Tiwari on 16/04/14.
+//  Copyright (c) 2014 MacPC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AlarmClockTest : NSObject
+
+@end
---
+++ AlarmClock/AlarmClockTest.m
@@ -0,0 +1,13 @@
+//
+//  AlarmClockTest.m
+//  AlarmClock
+//
+//  Created by Uma Shanker Tiwari on 16/04/14.
+//  Copyright (c) 2014 MacPC. All rights reserved.
+//
+
+#import "AlarmClockTest.h"
+
+@implementation AlarmClockTest
+
+@end
